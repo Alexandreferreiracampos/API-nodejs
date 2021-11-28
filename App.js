820b1607556c182b9cef4de34aa9bca6006a511e
AlexandreCampos <alexandrerazrmaxx@gmail.com>
2021-11-28 09:47:14 -0400

    Retornar dados solicitados para alguns dispositovo que solicitar com algum valor informado pelo metodo get

--- App.js
+++ App.js
@@ -24,7 +24,7 @@ app.get("/1", (req, res) =>{
     res.send("Introdução a api");
 });
 
-
+//solicitar todos dados salvos no banco de dados
 app.get("/", (req, res) =>{
   //return res.json({titulo: "Como criar uma API"});
   Artigo.find({}).then((artigo) =>{
@@ -34,7 +34,44 @@ app.get("/", (req, res) =>{
   })
 });
 
+//solicitar um dados especificado dentro do banco de dados utilizando _ID
+app.get("/artigo/:id", (req, res)=>{
+  console.log(req.params.id)
 
+  Artigo.findOne({_id:req.params.id}).then((artigo)=>{
+    return res.json(artigo);
+  }).catch((erro)=>{
+    return res.status(400).json({error: true, message:"Nenhuem artigo encontardo"})
+  })
+  
+})
+
+//solicitar somente 1 dado pelo cpf ou nome
+app.get("/cpf/:cpf", (req, res)=>{
+  console.log(req.params.cpf);
+
+  Artigo.findOne({cpf:req.params.cpf}).then((artigo)=>{
+    return res.json(artigo);
+  }).catch((erro)=>{
+    return res.status(400).json({error: true, message:"Nenhuem artigo encontardo"})
+  })
+  
+})
+
+//solicitar todos os dados com o mesmo cpf ou nome
+app.get("/nome/:nome", (req, res)=>{
+  console.log(req.params.nome);
+
+  Artigo.find({nome:req.params.nome}).then((artigo)=>{
+    return res.json(artigo);
+  }).catch((erro)=>{
+    return res.status(400).json({error: true, message:"Nenhuem artigo encontardo"})
+  })
+  
+})
+
+
+//salvar dados no banco de dados
 app.post("/artigo", (req, res) =>{
   const artigo = Artigo.create(req.body, (err) =>{
     if(err) return res.status(400).json({
